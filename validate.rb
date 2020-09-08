e017cc2f9be8f387171d8885669105e8c9c2edaa
Rafael Sales <rafael.sales93@hotmail.com>
2020-09-08 11:00:21 -0300

    Remove  global variables.

--- validate.rb
+++ validate.rb
@@ -4,27 +4,27 @@ require 'digest'
 require 'openssl'
 
 def token 
-  @key = 'sales-key'
+  key = 'sales-key'
 
-  @header = [
+  header = [
     'typ' => 'JWT',
     'alg' => 'HS256'
   ]
-  @header = JSON.generate(@header)
-  @header = Base64.encode64(@header).tr('+/', '-_').gsub(/[\n=]/, '')
+  header = JSON.generate(header)
+  header = Base64.encode64(header).tr('+/', '-_').gsub(/[\n=]/, '')
   
-  @payload = [
+  payload = [
     'iss' => 'salescreations.com.br',
     'username' => 'rsales',
     'email' => 'rafael.sales@salescreations.com.br'
   ]
-  @payload = JSON.generate(@payload)
-  @payload = Base64.encode64(@payload).tr('+/', '-_').gsub(/[\n=]/, '')
+  payload = JSON.generate(payload)
+  payload = Base64.encode64(payload).tr('+/', '-_').gsub(/[\n=]/, '')
   
-  @signature = OpenSSL::HMAC.digest('sha256', @key, "#{@header}.#{@payload}")
-  @signature =  Base64.encode64(@signature).tr('+/', '-_').gsub(/[\n=]/, '')
+  signature = OpenSSL::HMAC.digest('sha256', key, "#{header}.#{payload}")
+  signature =  Base64.encode64(signature).tr('+/', '-_').gsub(/[\n=]/, '')
   
-  return "#{@header}.#{@payload}.#{@signature}"
+  return "#{header}.#{payload}.#{signature}"
 end
 
 recived_token = 'W3sidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifV0.W3siaXNzIjoic2FsZXNjcmVhdGlvbnMuY29tLmJyIiwidXNlcm5hbWUiOiJyc2FsZXMiLCJlbWFpbCI6InJhZmFlbC5zYWxlc0BzYWxlc2NyZWF0aW9ucy5jb20uYnIifV0.xYTqn1qCIAjB23DCBAtXtpPkcEHkGJMx4XDlWD1aYe0'
